02fa52ee00604bab2792412aecdfe94157951dec
dxo <hi@d-xo.org>
2024-09-19 18:17:22 +0200

    semantics: try out an inductive approach

--- Act/AST.lean
+++ Act/AST.lean
@@ -10,25 +10,25 @@ namespace Act
 inductive AbiType where
 | UInt : Nat -> AbiType
 | Int  : Nat -> AbiType
-deriving Repr, DecidableEq
+deriving Repr, DecidableEq, BEq
 -- TODO other types
 
 inductive ValueType where
 | Contract  : String -> ValueType
 | Primitive : AbiType -> ValueType
-deriving Repr, DecidableEq
+deriving Repr, DecidableEq, BEq
 
 inductive SlotType where
 | Mapping : List ValueType -> ValueType -> SlotType
 | Value   : ValueType -> SlotType
-deriving Repr, DecidableEq
+deriving Repr, DecidableEq, BEq
 
 inductive StorageVar where
 | StorageVar : String -> SlotType -> StorageVar
-deriving Repr, DecidableEq
+deriving Repr, DecidableEq, BEq
 
 def Store := AList (λ (_ : String) => AList (λ (_ : String) => SlotType × Nat))
-deriving DecidableEq
+deriving DecidableEq, BEq
 
 instance : Repr Store where
   reprPrec s n := reprPrec (s.entries.map (λ v => (v.fst, v.snd.entries))) n
@@ -36,17 +36,38 @@ instance : Repr Store where
 instance : Repr ByteArray where
   reprPrec b n := reprPrec b.data n
 
+instance : BEq ByteArray where
+  beq l r := BEq.beq l.data r.data
+
 inductive ActType where
 | Bool
 | Int
 | ByteStr
 | Contract
-deriving Repr, DecidableEq
+deriving Repr, DecidableEq, BEq
+
+-- TODO: why can't I just use the LawfulBEq instance here:
+-- https://github.com/leanprover/lean4/blob/8f9843a4a5fe1b0c2f24c74097f296e2818771ee/src/Init/Core.lean#L635C1-L637C37
+@[simp]
+protected def ActTy.beq : ActType → ActType → Bool
+| .Bool, .Bool => true
+| .Int, .Int => true
+| .ByteStr, .ByteStr => true
+| .Contract, .Contract => true
+| _, _ => false
+
+@[simp]
+instance : BEq ActType := ⟨ActTy.beq⟩
+
+instance : LawfulBEq ActType where
+  rfl {a} := by induction a <;> simp_all
+  eq_of_beq {a b} := by
+    induction a generalizing b <;> (cases b <;> aesop)
 
 inductive When where
 | Pre
 | Post
-deriving Repr, DecidableEq
+deriving Repr, DecidableEq, BEq
 
 inductive EthEnv : ActType → Type where
 | Caller : EthEnv .Int
@@ -62,7 +83,7 @@ inductive EthEnv : ActType → Type where
 | Timestamp : EthEnv .Int
 | This : EthEnv .Int
 | Nonce : EthEnv .Int
-deriving Repr, DecidableEq
+deriving Repr, DecidableEq, BEq
 
 -- Lean can't automatically derive DecidableEq for nested inductive types yes:
 --  - https://github.com/leanprover/lean4/issues/2329
@@ -112,37 +133,37 @@ inductive Exp : ActType → Type where
 | ITE : Exp .Bool → Exp t → Exp t → Exp t
 | Var : (t : ActType) → AbiType → String → Exp t
 | Entry : When → StorageItem t → Exp t
-deriving Repr
+deriving Repr, BEq
 
 inductive TypedExp where
 | TExp : ∀ {ty}, Exp ty → TypedExp
-deriving Repr
+deriving Repr, BEq
 
 inductive StorageRef where
 | Var : String → String → StorageRef
 | Mapping : StorageRef → List TypedExp → StorageRef
 | Field : StorageRef → String → String → StorageRef
-deriving Repr
+deriving Repr, BEq
 
 inductive StorageItem : ActType → Type where
 | Item : (ty : ActType) → ValueType → StorageRef → StorageItem ty
-deriving Repr
+deriving Repr, BEq
 
 end
 
 inductive StorageUpdate where
 | Update : StorageItem ty → Exp ty → StorageUpdate
-deriving Repr
+deriving Repr, BEq
 
 structure Decl where
   type : AbiType
   name : String
-deriving Repr
+deriving Repr, BEq
 
 structure Interface where
   name : String
   decls : List Decl
-deriving Repr
+deriving Repr, BEq
 
 structure Constructor where
   name           : String
@@ -150,7 +171,7 @@ structure Constructor where
   preconditions  : List (Exp .Bool)
   postconditions : List (Exp .Bool)
   initStorage    : List StorageUpdate
-deriving Repr
+deriving Repr, BEq
 
 structure Behaviour where
   name           : String
@@ -160,24 +181,24 @@ structure Behaviour where
   postconditions : List (Exp .Bool)
   storageUpdates : List StorageUpdate
   returns        : Option TypedExp
-deriving Repr
+deriving Repr, BEq
 
 structure Invariant where
   contract : String
   preconditions : List (Exp .Bool)
   storageBounds : List (Exp .Bool)
   predicate : Exp .Bool × Exp .Bool
-deriving Repr
+deriving Repr, BEq
 
 structure Contract where
   constructor : Constructor
   behaviours : List Behaviour
   invariants : List Invariant
-deriving Repr
+deriving Repr, BEq
 
 structure Spec where
   store : Store
   contracts : List Contract
-deriving Repr
+deriving Repr, BEq
 
 end Act
--- Act/Semantics.lean
+++ Act/Semantics.lean
@@ -1,61 +1,159 @@
 -- give act expressions a concrete semantics in terms of the EVM
 
+import Mathlib.Control.Traversable.Basic
 import Mathlib.Data.List.AList
 
 import Act.AST
 
 namespace Act
 
+inductive Val where
+| Int : Int → Val
+| ByteStr : ByteArray → Val
+| Bool : Bool → Val
+deriving BEq
+
 @[simp]
-def rep : ActType -> Type
-| .Bool => Bool
-| .ByteStr => ByteArray
+def rep : (a : ActType) → Type
 | .Int => Int
--- TODO: what to do here?
+| .ByteStr => ByteArray
+| .Bool => Bool
 | .Contract => sorry
 
-inductive Val where
-| Val : {t : ActType} → rep t → Val
+def cast : (a : ActType) → Val → Option (rep a)
+| .Int, .Int v => some v
+| .ByteStr, .ByteStr v => some v
+| .Bool, .Bool v => some v
+| _, _ => none
 
-structure State where
-  env : EthEnv t → rep t
-  store : StorageRef → Val
+-- TODO: this name sucks. it should be SlotType, but that's taken...
+inductive LocType where
+| Mapping : ActType -> LocType -> LocType
+| Value   : ActType -> LocType
+deriving Repr, DecidableEq, BEq
 
-def Vars := AList (λ (_ : String) => Val)
+-- TODO: this name is bad too
+-- TODO: this restricts the types in a nice way but is very dependent, we might regret this later
+def rep' : LocType → Type
+| .Mapping arg ret => rep arg → rep' ret
+| .Value ty => rep ty
 
-def eval (s : State) (v : Vars) : Exp a → rep a
-| .And l r => (eval s v l) && (eval s v r)
-| .Or l r => (eval s v l) || (eval s v r)
+def extractTy : TypedExp → ActType
+| @TypedExp.TExp t _ => t
 
-| .Add l r => Int.add (eval s v l) (eval s v r)
-| .Sub l r => Int.sub (eval s v l) (eval s v r)
-
--- TODO: how to do this?
---| .Eq l r => eval s v l == eval s v r
-
-| .Var ty _ nm => match v.lookup nm with
-  | .some (@Val.Val t v) => match decEq t ty with
-    | isTrue h => h ▸ v
-    -- TODO: what to do about these unreachable cases...
-    | isFalse _ => sorry
-  | .none => sorry
-
+def locTy (rt : ActType) : StorageRef → LocType
+| .Var _ _ => .Value rt
+| .Mapping r args => args.foldl (λ acc arg => .Mapping (extractTy arg) acc) (.Value rt)
 | _ => sorry
 
--- TODO: handle returns
-def step (behv : Behaviour) (state : State) : State := ⟨ state.env, store' ⟩
-  where
-    -- TODO: construct from sig
-    vars := sorry
-    cond := List.map (eval state vars) (behv.preconditions ++ behv.caseconditions)
-         |> (List.all · id)
-    store' :=
+inductive StorageVal : Type where
+| Val : (ty : LocType) → rep' ty → StorageVal
+
+abbrev Storage := String → Option StorageVal
+
+def applyUpdate (store : Storage) (ref : StorageRef) (t : ActType) (val : rep t) : Storage :=
+   -- TODO: brain hurts...
+   λ k => if k == k then some (.Val (locTy t ref) sorry) else store k
+
+structure State where
+  env : ∀ t, EthEnv t → Val
+  vars : String → Option Val
+  store : Storage
+
+namespace ind
+
+inductive eval : State → Exp a → rep a → Type where
+| And
+  : eval s x rx
+  → eval s y ry
+  → eval s (.And x y) (Bool.and rx ry)
+
+| LitInt : ∀ s i, eval s (.LitInt i) i
+
+| Add
+  : eval pre x rx
+  → eval pre y ry
+  → eval pre (.Add x y) (Int.add rx ry)
+
+| Var :
+  ∀ nm val ty ret abi
+  , pre.vars nm = some val
+  → cast ty val = some ret
+  → eval pre (.Var ty abi  nm) ret
+
+inductive evalTExp : State → TypedExp → (Σ a, rep a) → Type where
+| TExp : ∀ ty e (rty : rep ty), eval s e rty → evalTExp s (@TypedExp.TExp ty e) ⟨ty, rty⟩
+
+inductive Updates : State → State → List StorageUpdate → State → Type where
+| Base : ∀ pre, Updates pre acc [] acc
+| App
+  : ∀ pre acc (e : Exp ta)
+  , eval pre e v
+  → Updates pre acc tl post
+  → Updates pre post ((.Update (.Item ta tv ref) e) :: tl) ⟨pre.env, pre.vars, applyUpdate pre.store ref ta v⟩
+
+inductive step : Behaviour → State → State → (Σ a, rep a) → Type where
+| Step
+   : ∀ name iface pres cases posts updates retexp
+   , ∀ (e : Exp .Bool), e ∈ pres → eval s e true
+   → ∀ (e : Exp .Bool), e ∈ cases → eval s e true
+   -- TODO: state updates
+   -- for each update,
+   → evalTExp pre retexp rval
+   → step ⟨name, iface, pres, cases, posts, updates, retexp⟩ pre post rval
+
+def hi (s : State) := eval.Add (.LitInt s 10) (.LitInt s 2)
+
+end ind
+
+namespace int
+
+def eval (s : State) : Exp a → Option Val
+| .And l r => match eval s l, eval s r with
+  | .some (.Bool l'), .some (.Bool r') => .some (.Bool (l' && r'))
+  | _, _ => .none
+
+| .Add l r => match eval s l, eval s r with
+  | .some (.Int l'), .some (.Int r') => .some (.Int (l' + r'))
+  | _, _ => .none
+
+| .Cat l r => match eval s l, eval s r with
+  | .some (.ByteStr l'), .some (.ByteStr r') => .some (.ByteStr (l' ++ r'))
+  | _, _ => .none
+
+| .Eq l r => match eval s l, eval s r with
+  | .some (.Int l'), .some (.Int r') => .some (.Bool (l' == r'))
+  | .some (.ByteStr l'), .some (.ByteStr r') => .some (.Bool (l' == r'))
+  | .some (.Bool l'), .some (.Bool r') => .some (.Bool (l' == r'))
+  | _, _ => .some (.Bool false)
+
+| _ => .none
+
+
+def step (behv : Behaviour) (state : State) : Option (State × Option Val) := do
+  let cond ← do
+    let vs ← sequence $ List.map (eval state) (behv.preconditions ++ behv.caseconditions)
+    let bs ← sequence $ vs.map (λ v => match v with
+      | .Bool b => some b
+      | _ => none)
+    pure $ bs.all id
+
+  let ret := match behv.returns with
+    | .some (.TExp e) => eval state e
+    | .none => .none
+
+  let store' :=
       if not cond
       then state.store
       else behv.storageUpdates.foldl (λ store upd => match upd with
-        | .Update (.Item t _ ref) exp =>
-          -- TODO: fails because we don't have DecidableEq for StorageRef...
-          (λ r => if r = ref then @Val.Val t (eval state vars exp) else store r)
+        | .Update (.Item t _ ref) exp => match eval state exp with
+          | .none => store
+          | .some v => λ r => if r == ref then v else store r
       ) state.store
+
+  pure ⟨⟨ state.env, state.vars, store' ⟩, ret⟩
+
+end int
+
 
 end Act
